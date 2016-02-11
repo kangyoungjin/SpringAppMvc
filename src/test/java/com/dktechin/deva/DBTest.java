f97fc7df63957910f9d2132836cda4de27260601
deva-jin <deva.jin@kakaocorp.com>
2016-02-11 16:25:24 +0900

    test ...

---
+++ src/test/java/com/dktechin/deva/DBTest.java
@@ -0,0 +1,20 @@
+package com.dktechin.deva;
+
+import javax.inject.Inject;
+import javax.sql.DataSource;
+
+import org.junit.Test;
+
+public class DBTest extends SpringTest {
+	
+	@Inject
+	DataSource ds;
+	
+	@Test
+	public void testDb() throws Exception{
+		log.info("-------------------------");
+		log.info(ds.getConnection());
+		log.info("-------------------------");		
+	}
+	
+}
