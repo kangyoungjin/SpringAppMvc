e19d72b3000fd1967af5040b55b2f6c66a54ca99
deva-jin <deva.jin@kakaocorp.com>
2016-02-11 16:25:24 +0900

    test ...

---
+++ src/test/java/com/dktechin/deva/MapperTest.java
@@ -0,0 +1,22 @@
+package com.dktechin.deva;
+
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.junit.Test;
+
+public class MapperTest extends SpringTest {
+	
+	@Inject
+	SqlSessionFactory sf;
+	
+	@Test
+	public void sfTest() throws Exception{
+		SqlSession session = sf.openSession();
+		log.info("---------------------------");
+		log.info(session.selectOne("com.dktechin.deva.mappers.Timmer.getTime"));
+		log.info("---------------------------");
+	}
+	
+}
---
+++ src/test/java/com/dktechin/deva/SpringTest.java
@@ -0,0 +1,28 @@
+package com.dktechin.deva;
+
+import javax.inject.Inject;
+import org.apache.log4j.Logger;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"file:src/main/**/*-context.xml"})
+@TestExecutionListeners({DependencyInjectionTestExecutionListener.class})
+public class SpringTest {
+
+	protected Logger log = Logger.getLogger(this.getClass());
+	
+	@Inject
+	String str;
+	
+	@Test
+	public void test() throws Exception{
+//		log.info("----------------------------");
+//		log.info(str);
+//		log.info("----------------------------");
+	}
+}
